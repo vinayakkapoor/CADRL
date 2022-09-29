6c5b3f09498e2f056ec4a5b04cbe23a9ad2ed4af
Vinayak <vinayakkapoor12@gmail.com>
2022-09-29 13:03:05 +0200

    trained and tested on switchPos for gamma 0.4 and 0.3

---
+++ run_2.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 train.py --config=./configs/model_Sep27_switchPos_gamma03_cap100_2.config  --gpu
+python3 test.py --config=./configs/model_Sep27_switchPos_gamma03_cap100_2.config  --gpu
---
+++ run_5.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 train.py --config=./configs/model_Sep27_switchPos_gamma04_cap100_1.config  --gpu
+python3 test.py --config=./configs/model_Sep27_switchPos_gamma04_cap100_1.config  --gpu
