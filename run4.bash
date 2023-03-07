3a55105cf08c96fa5459dcd1bab090ef9375392b
Vinayak <vinayakkapoor12@gmail.com>
2023-03-07 20:21:45 +0530

    cleaning the repo

--- run4.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 test.py --config=./configs/model_Sep30_p05_gamma05_cap10000_batch_1000_4.config --gpu
-python3 test.py --config=./configs/model_Sep30_p07_gamma05_cap10000_batch_1000_4.config --gpu
-python3 test.py --config=./configs/model_Sep30_p09_gamma05_cap10000_batch_1000_4.config --gpu
